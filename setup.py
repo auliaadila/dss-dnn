9af2dc159f246b249db84ec2d6b370fbef1f1420
auliaadila <auliaadila36@gmail.com>
2025-07-10 14:03:55 +0900

    setup.py for component import

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dss_dnn_project",
+    version="0.1",
+    packages=find_packages(),  # will automatically pick up components, tools, dss, etc.
+)
